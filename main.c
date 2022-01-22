2c13391edc33b378e8a09915d2c5a12eb6a2bb13
Jonas-Bergmann <bergmann.jonas31@gmail.com>
2022-01-22 13:12:08 +0100

    implemented skeleton for basic dpda construction and usage

--- main.c
+++ main.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define STACK_CAP 1024
-
-typedef struct _Stack {
-	int values[STACK_CAP];
-} Stack;
+#define STATE_CAP 64
 
 typedef struct _DPDA {
-	Stack stack;
-} DPDA;
+	int stack[STACK_CAP];
+	size_t stackHead;
+	
+	int transitions[STATE_CAP][STATE_CAP];
 
+	char* input;
+	size_t inputHead;
+} DPDA;
 
 int main(int argc, char **argv) {
 	DPDA dpda;
-	for (int i = 0; i < STACK_CAP; i++) {
-		dpda.stack.values[i] = i;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			dpda.transitions[i][j] = i + j;
+		}
 	}
-	for (int i = 0; i < STACK_CAP; i++) {
-		printf("%d,", dpda.stack.values[i]);
+
+	printf("%d\n", dpda.stack[0]);
+	printf("%d\n", dpda.stack[1]);
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("%d,", dpda.transitions[i][j]);
+		}
+		printf("\n");
 	}
+
 }
